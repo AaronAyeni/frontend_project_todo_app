0e12a4468a8ae96cc3f31a7bd7f0333b6b622d49
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-07-08 14:37:16 +0100

    added superuser in data loader

--- backend_project_todo_app/src/main/java/com/group_one/todo_list/components/DataLoader.java
+++ backend_project_todo_app/src/main/java/com/group_one/todo_list/components/DataLoader.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+import static com.group_one.todo_list.models.Category.COOKING;
+
 @Component
 public class DataLoader implements ApplicationRunner {
 
@@ -36,17 +38,19 @@ public class DataLoader implements ApplicationRunner {
         User ghishani = new User("Ghishani", Category.CLEANING,igloo, 23);
         User tom = new User("Tom", Category.LAUNDRY, blueHouse, 24);
         User adil = new User("Adil", Category.HOOVERING, blueHouse, 29);
-        User gellila = new User("Gellila", Category.COOKING, palace, 23);
-        User father = new User("John", Category.COOKING, house, 50);
+        User gellila = new User("Gellila", COOKING, palace, 23);
+        User father = new User("John", COOKING, house, 50);
         User mother = new User("Sarah", Category.CLEANING, house, 54);
         User son = new User("Bob", Category.HOOVERING, house, 14);
         User daughter = new User("Bobbie", Category.LAUNDRY, house,12);
 
+        User superUser = new User("Admin", Category.COOKING, house, 24);
+
         Task task1 = new Task("Wipe the kitchen countertops", Category.CLEANING, LocalDate.of(2024, 6, 18),blueHouse);
         Task task2 = new Task("Put the clothes in the washing machine", Category.LAUNDRY, LocalDate.of(2024, 6, 16),igloo);
         Task task3 = new Task("Hoover the living room", Category.HOOVERING, LocalDate.of(2024, 6, 15),blueHouse);
-        Task task4 = new Task("Cook lunch", Category.COOKING, LocalDate.of(2024, 6, 13), palace);
-        Task task5 = new Task("Cook dinner", Category.COOKING, LocalDate.of(2024, 6, 14), house);
+        Task task4 = new Task("Cook lunch", COOKING, LocalDate.of(2024, 6, 13), palace);
+        Task task5 = new Task("Cook dinner", COOKING, LocalDate.of(2024, 6, 14), house);
         Task task6 = new Task("Tidy the bedrooms", Category.CLEANING, LocalDate.of(2024, 6, 12), house);
         Task task7 = new Task("Hoover up the stairs", Category.HOOVERING, LocalDate.of(2024, 6, 11), house);
         Task task8 = new Task("Put the clothes in the dryer", Category.LAUNDRY, LocalDate.of(2024, 6, 10), house);
@@ -65,6 +69,8 @@ public class DataLoader implements ApplicationRunner {
         userRepository.save(mother);
         userRepository.save(son);
         userRepository.save(daughter);
+        userRepository.save(superUser);
+
 
         taskRepository.save(task1);
         taskRepository.save(task2);
