88e545188b0641fc57f9d951cca4d8de8cd3c9fc
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-07-09 18:05:07 +0100

    added household form

--- HomeChamp/src/components/Household.jsx
+++ HomeChamp/src/components/Household.jsx
@@ -1,14 +1,26 @@
-const Household = ({household}) => {
-console.log(household);
-    return(
-        <>
-        <h4>This is a household component</h4>
-        <ul>
-            <li>{household.name}</li>
-            <li>{household.users.map( (user) => {return user.name})}</li>
-        </ul>
-        </>
-    )
+import React from 'react';
+const Household = ({ household }) => {
+  console.log(household);
+  return (
+    <>
+      <h4>This is a household component</h4>
+      <ul>
+        <li>{household.name}</li>
+        <p>Users in this household</p>
+        <li>
+          {household && household.users
+            ? household.users.map((user, index) => (
+                <span key={index}>{user.name} </span>
+              ))
+            : "No users"}
+        </li>
+      </ul>
+    </>
+  );
 }
-
 export default Household;
+
+
+
+
+
--- HomeChamp/src/components/forms/AddHouseholdForm.jsx
+++ HomeChamp/src/components/forms/AddHouseholdForm.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import Household from "../Household";
 
-const AddHouseholdForm = () => {
+const AddHouseholdForm = ({householdData, postHousehold}) => {
 
+    const [householdName,setHouseholdName]  = useState("");
+
+
+    const handleSubmit = (event) => {
+        console.log("is the form submitting")
+        event.preventDefault();
+        const newHousehold = {
+            householdName
+    
+        }
+        postHousehold(newHousehold);
+    
+    }
 
 
     return(
         <>
          <h2>This is a household form</h2>
+
+         <form onSubmit={handleSubmit}>
+         <label> name:</label>
+         <input type = "text" id = "name"  onChange={(event) => setHouseholdName(event.target.value)} placeholder="Enter name of household"></input>
+         <input type="submit" value = "Add household"/>
+         </form>
         
         </>
        
