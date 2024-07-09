f682ec6a60f86e07870740f63d3a9c68ad5b7d97
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-07-09 16:22:15 +0100

    adding a post mapping and saving to the new user to the database

--- HomeChamp/src/containers/UserContainer.jsx
+++ HomeChamp/src/containers/UserContainer.jsx
@@ -2,14 +2,14 @@ import User from "../components/User";
 import AddUserForm from "../components/forms/AddUserForm";
 import UserList from "../components/lists/UserList";
 
-const UserContainer = ({userData, categoryData, householdData}) => {
+const UserContainer = ({userData, categoryData, householdData,postUser}) => {
 
     return(
         <>
 
         <h2>This is UserContainer</h2>
         
-        <AddUserForm userData={userData} categoryData ={categoryData} householdData={householdData}></AddUserForm>
+        <AddUserForm userData={userData} categoryData ={categoryData} householdData={householdData} postUser = {postUser}></AddUserForm>
         <UserList userData ={userData}></UserList>
 
 
