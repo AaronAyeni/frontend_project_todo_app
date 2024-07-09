a508c932e9e5a99bd3a62237f85d58a89bc56549
Aaron Ayeni <aaronayeni135@gmail.com>
2024-07-09 14:48:04 +0100

    fixed dropdown menu for user preference

--- HomeChamp/src/containers/UserContainer.jsx
+++ HomeChamp/src/containers/UserContainer.jsx
@@ -2,14 +2,14 @@ import User from "../components/User";
 import AddUserForm from "../components/forms/AddUserForm";
 import UserList from "../components/lists/UserList";
 
-const UserContainer = ({userData}) => {
+const UserContainer = ({userData, categoryData}) => {
 
     return(
         <>
 
         <h2>This is UserContainer</h2>
         
-        <AddUserForm userData={userData}></AddUserForm>
+        <AddUserForm userData={userData} categoryData ={categoryData}></AddUserForm>
         <UserList userData ={userData}></UserList>
 
 
