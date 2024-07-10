09990f13053f44f02cde112f747b8beba1ee3b0e
Aaron Ayeni <aaronayeni135@gmail.com>
2024-07-10 10:40:48 +0100

    crested and rendered task list

--- HomeChamp/src/components/Task.jsx
+++ HomeChamp/src/components/Task.jsx
@@ -1,8 +1,20 @@
-const Task = () => {
+const Task = ({task}) => {
 
 
     return(
+        <>
         <h4>This is a task component</h4>
+        <ul>
+          <li>{task.description}</li>
+          <li>{task.household.name}</li>
+          <li>{task.dueDate}</li>
+          <li>{task.category}</li>
+          <li>{task.status}</li>
+          <li>{task.user == null ? "no user associated with task" : task.user} </li>  
+
+
+        </ul>
+        </>
     )
 }
 
