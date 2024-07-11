c99fce314acf022e7f4c25e2c9cd5d1ca5477460
Aaron Ayeni <aaronayeni135@gmail.com>
2024-07-11 11:20:28 +0100

    added functionality to filter householdlist by household name inputed

--- HomeChamp/src/components/Household.jsx
+++ HomeChamp/src/components/Household.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 const Household = ({ household }) => {
 
 
-  if(household.users == null){
-    console.log("no users found");
-  }
-
-  console.log(household);
   return (
     <>
       <h4>This is a household component</h4>
       <ul>
         <li>{household.name}</li>
         <p>Users in this household</p>
-        <li>{household.users.map( (user) => {return user.name + " "})}</li>
+        <li>{household.users ? household.users.map(user => user.name + " ") : "No users found"}</li>
       </ul>
     </>
   );
--- HomeChamp/src/containers/HouseholdContainer.jsx
+++ HomeChamp/src/containers/HouseholdContainer.jsx
@@ -1,22 +1,28 @@
+import { useState, useEffect } from "react";
 import Household from "../components/Household";
 import AddHouseholdForm from "../components/forms/AddHouseholdForm";
 import HouseholdList from "../components/lists/HouseholdList";
 
 const HouseHoldContainer = ({householdData, postHousehold}) => {
 
+    const [filteredHouseholds, setFilteredHouseholds] = useState(householdData);
 
+    useEffect(() => {
+        setFilteredHouseholds(householdData);
+    }, [householdData]);
 
-
-
-
+    const handleFilterChange = (searchTerm) => {
+        const filtered = householdData.filter(household =>
+            household.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredHouseholds(filtered);
+    };
 
     return(
 
         <>
-        <h2>This is Household container</h2>
-
-       <AddHouseholdForm householdData={householdData} postHousehold ={postHousehold}></AddHouseholdForm>
-       <HouseholdList householdData={householdData}></HouseholdList>
+       <AddHouseholdForm householdData={householdData} postHousehold ={postHousehold} onFilterChange={handleFilterChange} ></AddHouseholdForm>
+       <HouseholdList householdData={filteredHouseholds}></HouseholdList>
         </>
         
     )
